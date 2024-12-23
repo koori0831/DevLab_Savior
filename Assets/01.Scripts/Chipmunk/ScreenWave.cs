3a35b4506a783be2592e242577edb2fa354d3fdc
Chipmunk-Rex <rexgood1005@gmail.com>
2024-12-23 12:16:32 +0900

    feat : shockWave Shader

--- Assets/01.Scripts/Chipmunk/ScreenWave.cs
+++ Assets/01.Scripts/Chipmunk/ScreenWave.cs
@@ -1,19 +1,39 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class ScreenWave : MonoBehaviour
 {
-    SpriteRenderer spriteRenderer;
-    [SerializeField] string propertyName;
-    int propertyId;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] bool destroyOnEnd = true;
+    int startTimeID = Shader.PropertyToID("_StartTime");
+    int strengthID = Shader.PropertyToID("_Strength");
+    int durationID = Shader.PropertyToID("_Duration");
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        propertyId = Shader.PropertyToID(propertyName);
+        startTimeID = Shader.PropertyToID("_StartTime");
+        strengthID = Shader.PropertyToID("_Strength");
     }
+    [SerializeField] float testStrength = 1;
+    [SerializeField] float testDuration = 1;
     [ContextMenu("wave")]
     public void Wave()
     {
-        float time = Time.time;
-        spriteRenderer.material.SetFloat(propertyId, time);
+        StartShockWave(testStrength, testDuration);
+    }
+    public void StartShockWave(float strength, float duration)
+    {
+        spriteRenderer.material.SetFloat(startTimeID, Time.time);
+        spriteRenderer.material.SetFloat(strengthID, strength * 0.1f);
+        spriteRenderer.material.SetFloat(durationID, duration);
+        if (destroyOnEnd)
+            StartCoroutine(DestroyAfter(duration));
+    }
+
+    private IEnumerator DestroyAfter(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Destroy(gameObject);
     }
 }
