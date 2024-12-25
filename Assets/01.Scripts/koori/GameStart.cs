fcfb782ce949e8e62ccfe82cda223c9364fa88f9
koori0831 <fuyunokoori0831@gmail.com>
2024-12-26 00:25:04 +0900

    add : escMenu

--- Assets/01.Scripts/koori/GameStart.cs
+++ Assets/01.Scripts/koori/GameStart.cs
@@ -5,7 +5,7 @@ public class GameStart : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("MainGame");
+        FadeManager.Instance.FadeSceneChange("MainScene", 1);
     }
 
     public void QuitGame()
