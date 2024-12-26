1a2e618f6275bc80ba0d5ef4c96c94dfe187a57a
koori0831 <fuyunokoori0831@gmail.com>
2024-12-26 11:10:24 +0900

    feat : Rank

--- Assets/01.Scripts/Chipmunk/UI/Canvas/Leaderboard_PlayerUI.cs
+++ Assets/01.Scripts/Chipmunk/UI/Canvas/Leaderboard_PlayerUI.cs
@@ -9,12 +9,14 @@ public class Leaderboard_PlayerUI : MonoBehaviour
     [SerializeField] TMP_Text playerName;
     [SerializeField] TMP_Text playerScore;
     [SerializeField] TMP_Text playerID;
-    public void SetPlayerUI(string name, string score, string id)
+    [SerializeField] TMP_Text playerRank;
+    public void SetPlayerUI(string name, string score, string id, int rank)
     {
         int time = int.Parse(score);
         playerName.text = name;
         playerScore.text = $"{time/60}분 {time%60}초";
         playerID.text = $"ID : {id}";
+        playerRank.text = rank.ToString();
     }
 
 }
